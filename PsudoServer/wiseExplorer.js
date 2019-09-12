b16ea76cc67efb47bc10cafc7d059d05c276a43a
Denise Escobar <descobar1490@gmail.com>
2019-09-11 22:46:02 -0500

    Working treasure code. stops once you hit 9 encumbrance

--- PsudoServer/wiseExplorer.js
+++ PsudoServer/wiseExplorer.js
@@ -16,6 +16,7 @@ let encumbrance = 0
 let gold = 0
 let inventory = []
 
+//Chooses a random direction based of available exits in current room
 randomDirection= (obj) => {
     var keys = Object.keys(obj)
     return keys[ keys.length * Math.random() << 0];
@@ -50,26 +51,34 @@ init = () =>{
     encumbrance = status.encumbrance
     gold = status.gold
     inventory = status.inventory
-    //console.log(encumbrance, strength)
     })
     .catch(err => console.error(err.response));
 
-    if(encumbrance < strength -1){
-
-    }
 
 }
 
 adventure = () => {
-    
 
-    //while(encumbrance < strength -1){
-        if(!Object.keys(mapData[currentRoom.room_id]).includes(randDirection)){
-            console.log(randDirection, Object.keys(mapData[currentRoom.room_id]) )
-            randDirection = randomDirection(mapData[currentRoom.room_id])
-            console.log("new direction", randDirection)
-        }
+    //TODO: add ability to pick up more than once piece of treasure
+    //Check player status before each search to check encumbrance
+    treasureHunt
+    .post("status")
+    .then(res => {
+    console.log("status: ", res.data);
+    status = res.data;
+    strength = status.strength
+    encumbrance = status.encumbrance
+    gold = status.gold
+    inventory = status.inventory
+    })
+    .catch(err => console.error(err.response));
+    
+    //if encumbrance is less than strength use wise explorer and search for treasure
+    if(encumbrance < strength -2 ){
+        //set a random direction of travel and get the next room based of mapData
+        randDirection = randomDirection(mapData[currentRoom.room_id])
         var nextRoom = mapData[currentRoom.room_id][randDirection]
+
         setTimeout(() =>{
             console.log("exploring")
             treasureHunt.post("move", { "direction":randDirection, "next_room_id": nextRoom.toString() })
@@ -77,10 +86,11 @@ adventure = () => {
                 coolDown = res.data.cooldown
                 currentRoom = res.data
                 console.log(res.data)
+
                 if(res.data.items.length){
                     console.log("FOUND TREASURE")
                     setTimeout(()=>{
-                        treasureHunt.post("take", {"name":items[0]})
+                        treasureHunt.post("take", {"name":res.data.items[0]})
                         .then((res) =>{
                             console.log(res.data)
                             setTimeout(() =>{
@@ -101,7 +111,9 @@ adventure = () => {
                 console.log(err.response)
             })
         }, coolDown * 1000)
-    //}
+    } else {
+        //go to store
+    }
 
 }
 
